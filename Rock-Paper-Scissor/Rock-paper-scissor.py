826b72b052490d0e9fee5e9a15417a11945bae0d
Moukhtar-youssef <moukhtar.youssef06@gmail.com>
2022-05-12 13:39:55 +0400

    Modifying some stuff 🔥🔥🔥

--- Rock-Paper-Scissor/Rock-paper-scissor.py
+++ Rock-Paper-Scissor/Rock-paper-scissor.py
@@ -1,11 +1,32 @@
 import random
+from time import sleep
 userinput = input("do you want to play a game? [Y/N] :")
-if (userinput == "y" or userinput == "Y"):
-    print("okay cool \n we are going to play Rock paper scissor")
-    print("ready")
-    print("Rock")
-    print("Paper")
-    print("Scissor")
-    print("Shoot")
+
+
+def main():
+    if (userinput == "y" or userinput == "Y"):
+        print("okay cool \n we are going to play Rock paper scissor")
+        sleep(1)
+        print("ready")
+        sleep(1)
+        print("Rock")
+        sleep(1)
+        print("Paper")
+        sleep(1)
+        print("Scissor")
+        sleep(1)
+        print("Shoot")
+        sleep(1)
+        Rock_Paper_Scissor()
+
+
+def Rock_Paper_Scissor():
     userinput = input("what did you choose: ")
-    Robot = random.choice(['Rock', 'paper', 'Scissor'])
+    Robot = random.sample(['Rock', 'paper', 'Scissor'], k=1)
+    if(userinput == Robot):
+        print('Oh it is a draw lets plat again')
+        sleep(1)
+        Rock_Paper_Scissor()
+
+
+main()
