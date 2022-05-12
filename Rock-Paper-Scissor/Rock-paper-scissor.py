b9504379510111db9ce55c1e863f2a8f98f07d16
Moukhtar-youssef <moukhtar.youssef06@gmail.com>
2022-05-12 14:26:32 +0400

    final project 🔥🔥🔥

--- Rock-Paper-Scissor/Rock-paper-scissor.py
+++ Rock-Paper-Scissor/Rock-paper-scissor.py
@@ -1,32 +1,27 @@
 import random
-from time import sleep
-userinput = input("do you want to play a game? [Y/N] :")
+from time import sleep, time
+print("lets play rock paper scissors where R is for rock and p for paper and s for scissors")
+
+
+def play():
+    Robot = random.choice(['r', 'p', 's'])
+    userinput = input("what did you choose [r , p , s]: ")
+    if userinput == Robot:
+        return 'it \'s a tie'
+    if is_win(userinput, Robot):
+        return 'you won!'
+    return 'You lost!'
+
+
+def is_win(userinput, Robot):
+    if (userinput == 'r' and Robot == 's') or (userinput == 's' and Robot == 'p') or (userinput == 'p' and Robot == 'r'):
+        return True
 
 
 def main():
-    if (userinput == "y" or userinput == "Y"):
-        print("okay cool \n we are going to play Rock paper scissor")
-        sleep(1)
-        print("ready")
-        sleep(1)
-        print("Rock")
-        sleep(1)
-        print("Paper")
-        sleep(1)
-        print("Scissor")
-        sleep(1)
-        print("Shoot")
-        sleep(1)
-        Rock_Paper_Scissor()
-
-
-def Rock_Paper_Scissor():
-    userinput = input("what did you choose: ")
-    Robot = random.sample(['Rock', 'paper', 'Scissor'], k=1)
-    if(userinput == Robot):
-        print('Oh it is a draw lets plat again')
-        sleep(1)
-        Rock_Paper_Scissor()
+    print(play())
+    sleep(2)
+    main()
 
 
 main()
